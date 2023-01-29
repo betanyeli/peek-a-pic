388a090018ffecc489215ce9b8ba8cad9fe8da4e
betanyeli <betangelii@gmail.com>
2023-01-29 00:07:57 -0300

    feat: add lottie, rn-paper, rn-image-picker, rn-vector-icons

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -3,22 +3,34 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CameraViewScreen from '../screens/camera-view/CameraViewScreen';
 import { Text, View } from 'react-native';
 import ResultViewScreen from '../screens/result-view/ResultViewScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Tab = createBottomTabNavigator();
 
 function Navigation() {
+    const CameraViewStack = createNativeStackNavigator();
+
+    function CameraViewStackScreen() {
+        return (
+            <CameraViewStack.Navigator>
+                <CameraViewStack.Screen name="CameraView" component={CameraViewScreen} />
+                <CameraViewStack.Screen name="ResultView" component={ResultViewScreen} />
+            </CameraViewStack.Navigator>
+        );
+    }
+
     return (
-        <Tab.Navigator>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
             <Tab.Screen options={({ navigation }) => ({
                 title: 'PeekAPic',
                 tabBarLabel: 'Get a pic',
                 tabBarIcon: () => <View><Text>Icon</Text></View>,
-            })} name="CameraView" component={CameraViewScreen} />
-            <Tab.Screen options={({ navigation }) => ({
+            })} name="Camera" component={CameraViewStackScreen} />
+            {/* <Tab.Screen options={({ navigation }) => ({
                 title: 'Result',
                 tabBarLabel: 'Result',
                 tabBarIcon: () => <View><Text>Icon</Text></View>,
-            })} name='ResultView' component={ResultViewScreen} />
+            })} name='ResultView' component={ResultViewScreen} /> */}
         </Tab.Navigator>
     );
 }
--- src/screens/result-view/ResultViewScreen.tsx
+++ src/screens/result-view/ResultViewScreen.tsx
@@ -1,12 +1,48 @@
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 import React from 'react';
 import styles from './ResultViewScreen.styles';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-const ResultViewScreen = () => {
+const ResultViewScreen = ({ route }: any) => {
+    const { response } = route.params
     return (
-        <View style={styles.container}>
-            <Text>Result Screen</Text>
-        </View>
+        <SafeAreaView style={{
+            flex: 1,
+            backgroundColor: '#000',
+        }}>
+            {response?.assets &&
+                response?.assets.map(({ uri }: { uri: string }) => (
+                    <View key={uri} style={{
+                        marginVertical: 24,
+                        alignItems: 'center'
+                    }}>
+                        <Image
+                            resizeMode="cover"
+                            resizeMethod="scale"
+                            style={{
+                                width: 200,
+                                height: 200,
+                            }}
+                            source={{ uri: uri || 'https://images.unsplash.com/photo-1618826411640-d6df44dd3f7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80' }}
+                        />
+                    </View>
+                ))}
+
+            {response?.errorCode && <View style={{
+                marginVertical: 24,
+                alignItems: 'center'
+            }}>
+                <Image
+                    resizeMode="cover"
+                    resizeMethod="scale"
+                    style={{
+                        width: 200,
+                        height: 200,
+                    }}
+                    source={{ uri: 'https://images.unsplash.com/photo-1618826411640-d6df44dd3f7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80' }}
+                />
+            </View>}
+        </SafeAreaView>
     );
 };
 
